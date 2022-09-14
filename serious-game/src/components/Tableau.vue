0bac3abc91d2f6a31f14c31b0ab6f7a01010bf2e
ours <duranmevlut97@gmail.com>
2022-09-14 15:53:26 +0200

    fait

--- serious-game/src/components/Tableau.vue
+++ serious-game/src/components/Tableau.vue
@@ -1,42 +1,24 @@
-<!-- export default{
-  data () {
-    return {
-      info: null
-    }
-  },
-  mounted () {
-    axios
-      .get('https://killer-cepegra.xyz/cockpit-ingrwf10/api/content/items/questions?sort=%7Bnumber%3A%22asc%22%7D')
-      .then(response => (this.info = response))
-  }
-} -->
-
 <script setup>
   import {ref} from 'vue'
   import {useGameStore} from '@/stores/game'
-import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import dice from './ButtonDice.vue'
-import axios from 'axios';
+  import WelcomeItem from './WelcomeItem.vue'
+  import dice from './ButtonDice.vue'
+  import axios from 'axios';
 
 const gameStore = useGameStore()
 
 const purple = ref([])
 
 
-
 const api = () => {
       axios
       .get('https://killer-cepegra.xyz/cockpit-ingrwf10/api/content/items/questions?sort=%7Bnumber%3A%22asc%22%7D')
       .then(response => {
-        console.log(response.data)
          purple.value = response.data
          gameStore.addQuestions(response.data)
-         //return purple;
-         console.log(purple.value)
       })
     }
-        
+
 
 api()
 
